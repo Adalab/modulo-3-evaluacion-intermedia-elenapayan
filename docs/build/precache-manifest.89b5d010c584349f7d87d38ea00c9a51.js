e0b21035aaab5a629b42fedf2e2dc9b0649f59fc
elenapayan <elena.payan.glez@gmail.com>
2019-12-20 10:20:01 +0100

    Push docs

---
+++ docs/build/precache-manifest.89b5d010c584349f7d87d38ea00c9a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876b08f4a28dafe5ca4ad0af2b38db8d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "044a3397fb9ffb2bdf28",
+    "url": "./static/css/main.fe3eecc1.chunk.css"
+  },
+  {
+    "revision": "07b1777043a3fe3a6461",
+    "url": "./static/js/2.cc852215.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.cc852215.chunk.js.LICENSE"
+  },
+  {
+    "revision": "044a3397fb9ffb2bdf28",
+    "url": "./static/js/main.e0b3ba9b.chunk.js"
+  },
+  {
+    "revision": "480db8ddb605650a2c3f",
+    "url": "./static/js/runtime-main.046da4c2.js"
+  }
+]);
